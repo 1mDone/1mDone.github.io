47fb3bbcf9beaaf696931d17e9bbd574dacf01e6
Eugene <e@fup.app>
2019-03-03 18:08:34 +0300

    cdn for scripts

--- js/function.js
+++ js/function.js
@@ -6,7 +6,7 @@
 	
 	/* Preloader Effect */
 	$window.load(function() {
-		$(".preloader").fadeOut(600);
+		$(".preloader").fadeOut(400);
     });
 
 	/* Sticky header */
@@ -35,52 +35,6 @@
 			}
 		}
 	});
-	
-	/* Contact form validation */
-	var $contactform=$("#contactForm");
-	$contactform.validator({focus: false}).on("submit", function (event) {
-		if (!event.isDefaultPrevented()) {
-			event.preventDefault();
-			submitForm();
-		}
-	});
-
-	function submitForm(){
-		/* Initiate Variables With Form Content*/
-		var name = $("#name").val();
-		var email = $("#email").val();
-		var phone = $("#phone").val();
-		var subject = $("#subject").val();
-		var message = $("#message").val();
-
-		$.ajax({
-			type: "POST",
-			url: "form-process.php",
-			data: "name=" + name + "&email=" + email + "&subject=" + subject + "&message=" + message + "&phone=" + phone,
-			success : function(text){
-				if (text == "success"){
-					formSuccess();
-				} else {
-					submitMSG(false,text);
-				}
-			}
-		});
-	}
-
-	function formSuccess(){
-		$contactform[0].reset();
-		submitMSG(true, "Message Sent Successfully!")
-	}
-
-	function submitMSG(valid, msg){
-		if(valid){
-			var msgClasses = "h3 text-left text-success";
-		} else {
-			var msgClasses = "h3 text-left text-danger";
-		}
-		$("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
-	}
-	/* Contact form validation end */
 	
 	/* Animate with wow js */
     new WOW({mobile:false}).init(); 
