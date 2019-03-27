b72307c9bc273d9bae3c548b8256851c94aaf2a7
Eugene <e@fup.app>
2019-03-28 01:45:24 +0300

    order form

--- js/function.js
+++ js/function.js
@@ -38,5 +38,51 @@
 
 	/* Animate with wow js */
     new WOW({mobile:false}).init(); 
+
+    /* quantity */
+    window.inputNumber = function(el) {
+
+    	const pricePerKg = 750;
+
+    	var totalEl = $('.price-tag');
+
+	    var min = el.attr('min') || false;
+	    var max = el.attr('max') || false;
+
+	    var els = {};
+
+	    els.dec = el.prev();
+	    els.inc = el.next();
+
+	    el.each(function() {
+	      init($(this));
+	    });
+
+	    function init(el) {
+
+	      els.dec.on('click', decrement);
+	      els.inc.on('click', increment);
+
+	      function decrement() {
+	        var value = el[0].value;
+	        value--;
+	        if(!min || value >= min) {
+	          el[0].value = value;
+	          totalEl[0].innerText = el[0].value * pricePerKg + " ₽";
+	        }
+	      }
+
+	      function increment() {
+	        var value = el[0].value;
+	        value++;
+	        if(!max || value <= max) {
+	          el[0].value = value++ ;
+	          totalEl[0].innerText = el[0].value * pricePerKg + " ₽";
+	        }
+	      }
+	    }
+  }
 	
 })(jQuery);
+
+inputNumber($('.input-number'));
